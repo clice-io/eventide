ea4beab613b8b7c950e27069632f881959edb6ca
ykiko <ykikoykikoykiko@gmail.com>
2025-12-28 00:43:48 +0800

    Revert "build: add xmake ci"

--- include/reflection/name.h
+++ include/reflection/name.h
@@ -4,7 +4,6 @@
 #include <string_view>
 
 #include "traits.h"
-#include "utils.h"
 
 namespace refl {
 
@@ -13,11 +12,19 @@ consteval auto type_name(bool qualified = false) {
     std::string_view name = std::source_location::current().function_name();
 #if __GNUC__ || __clang__
     std::size_t start = name.rfind("T =") + 3;
-    std::size_t end = name.rfind("]");
-    end = end == std::string_view::npos ? name.size() : end;
-    name = utils::trim(name.substr(start, end - start));
+    if(name[start] == ' ') {
+        start += 1;
+    }
+    std::size_t end = name.find_first_of(";]", start);
+    if(end == std::string_view::npos) {
+        end = name.size();
+    }
+    if(name[end - 1] == ' ') {
+        end -= 1;
+    }
+    name = std::string_view(name.data() + start, end - start);
 #elif _MSC_VER
-    std::size_t start = name.find("type_name<") + 10;
+    std::size_t start = name.find('<') + 1;
     std::size_t end = name.rfind(">(");
     name = std::string_view{name.data() + start, end - start};
     start = name.find(' ');
@@ -47,39 +54,29 @@ consteval auto enum_name() {
 #else
     static_assert(false, "Not supported compiler");
 #endif
-    name = utils::trim(name.substr(start, end - start));
+    name = name.substr(start, end - start);
     start = name.rfind("::");
     return start == std::string_view::npos ? name : name.substr(start + 2);
 }
 
-template <typename T>
-struct wrapper {
-    /// workaround for msvc, if no such wrapper, msvc cannot print the member name.
-    T value;
-
-    constexpr wrapper(T value) : value(value) {}
-};
-
-template <wrapper ptr>
+template <auto*>
 consteval auto field_name() {
     std::string_view name = std::source_location::current().function_name();
-#if __GNUC__ && (!__clang__)
+#if __GNUC__ || __clang__ && (!_MSC_VER)
     std::size_t start = name.rfind("::") + 2;
     std::size_t end = name.rfind(')');
-    name = utils::trim(name.substr(start, end - start));
-#elif __clang__ && (!_MSC_VER)
-    std::size_t start = name.rfind(".") + 1;
-    std::size_t end = name.find_first_of(";]}", start);
-    name = utils::trim(name.substr(start, end - start));
+    name = name.substr(start, end - start);
 #elif _MSC_VER
     std::size_t start = name.rfind("->") + 2;
     std::size_t end = name.rfind('}');
-    name = utils::trim(name.substr(start, end - start));
+    name = name.substr(start, end - start);
 #else
 #error "UNKNOWN COMPILER";
 #endif
-    start = name.rfind("::");
-    return start == std::string_view::npos ? name : name.substr(start + 2);
+    if(name.rfind("::") != std::string_view::npos) {
+        name = name.substr(name.rfind("::") + 2);
+    }
+    return name;
 }
 
 }  // namespace refl
